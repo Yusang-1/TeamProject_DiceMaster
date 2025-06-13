d25dfe4525c27a6c51747db12d53d3967a87e790
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2025-06-13 16:33:33 +0900

    feat : 오디오 기초 스크립트

---
+++ Assets/01.Scrips/AudioManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region 싱글톤 구현
+    private static AudioManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static AudioManager Instance
+    {
+        get
+        {
+            if( instance == null)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    SFXPool audioPool;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource battleAudioSource;
+    [SerializeField] AudioSource reactAudioSource;
+    AudioSource backgroundAudioSource;
+
+    private void Start()
+    {
+        audioPool = GetComponent<SFXPool>();
+        backgroundAudioSource = GetComponent<AudioSource>();
+    }
+
+    #region 단발성 오디오 재생 메서드
+    public void PlayAudioOnce(AudioClip[] audioClips, MagicSFXEnum enumSFX)
+    {
+        battleAudioSource.PlayOneShot(audioClips[(int)enumSFX]);
+    }
+
+    public void PlayAudioOnce(AudioClip[] audioClips, BuffSFXEnum enumSFX)
+    {
+        battleAudioSource.PlayOneShot(audioClips[(int)enumSFX]);
+    }
+
+    public void PlayAudioOnce(AudioClip[] audioClips, PyhsicsSFXEnum enumSFX)
+    {
+        battleAudioSource.PlayOneShot(audioClips[(int)enumSFX]);
+    }
+
+    public void PlayAudioOnce(AudioClip[] audioClips, ReactSFXEnum enumSFX)
+    {
+        reactAudioSource.PlayOneShot(audioClips[(int)enumSFX]);
+    }
+
+    public void PlayAudioOnce(AudioClip[] audioClips, UISFXEnum enumSFX)
+    {
+        audioSource.PlayOneShot(audioClips[(int)enumSFX]);
+    }
+    #endregion
+
+    public void PlayBackGroundAudioOnStart(int stage)
+    {
+        StopAllCoroutines();
+        IEnumerator changeAudio;
+
+        int randNum = GetRandomClipNum(stage);
+
+        backgroundAudioSource.clip = audioPool.BackGroundAudio[stage][randNum];
+
+        changeAudio = ChangeAudio(stage);
+        StartCoroutine(changeAudio);
+    }
+
+    private int GetRandomClipNum(int stage)
+    {
+        int randNum = UnityEngine.Random.Range(0, audioPool.BackGroundAudio[stage].Length);
+        return randNum;
+    }
+
+    IEnumerator ChangeAudio(int stage)
+    {
+        while (true)
+        {
+            if(backgroundAudioSource.isPlaying == true)
+            {
+                yield return new WaitForSecondsRealtime(2.5f);
+            }
+            int randNum = GetRandomClipNum(stage);
+
+            backgroundAudioSource.clip = audioPool.BackGroundAudio[stage][randNum];
+            backgroundAudioSource.Play();
+            yield return new WaitForSecondsRealtime(50f);
+        }
+    }
+
+
+}
--- Assets/01.Scrips/SFXPool.cs
+++ Assets/01.Scrips/SFXPool.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,60 @@ public enum MagicSFXEnum
     Fire,
     Ice,
     Thunder,
-    Water,
     Wind,
-    Earth,
-    Charge,
-    Poision
 }
+
+public enum BuffSFXEnum
+{
+    Heal,
+    AtkBuff,
+    DefBuff,
+    Debuff
+}
+
+public enum PyhsicsSFXEnum
+{
+    Slash,
+    Bite    
+}
+public enum ReactSFXEnum
+{
+    Hit,
+    Evade,
+    Death
+}
+
+public enum UISFXEnum
+{
+    Confirm,
+    Denied,
+    UseItem,
+    Equip,
+    Unequip,
+    BuySell,
+    Pause,
+    Unpause
+}
+
 public class SFXPool : MonoBehaviour
 {
-    public List<AudioClip> MagicSFXClips = new List<AudioClip>();
+    public AudioClip[] MagicSFXClips = new AudioClip[4];
+    public AudioClip[] BuffSFXClips = new AudioClip[4];
+    public AudioClip[] PhysicsSFXClips = new AudioClip[2];
+    public AudioClip[] ReactSFXClips = new AudioClip[3];
 
+    public AudioClip[] UISFXClips = new AudioClip[8];
 
+    public AudioClip Walk;
+    
+    public AudioClip[] Stage1Audio = new AudioClip[4];
+    public AudioClip[] Stage2Audio = new AudioClip[4];
+
+    public AudioClip[][] BackGroundAudio = new AudioClip[2][];
+
+    private void Awake()
+    {
+        BackGroundAudio[0] = Stage1Audio;
+        BackGroundAudio[1] = Stage2Audio;
+    }
 }
